4ec4b8a259495edde20937dd86e628e34b35ea8a
Ethan Zhang <ethan.zhang@ringcentral.com>
2022-04-25 16:15:47 +0800

    refactor(*) Removes sagas

--- src/modules/production/production.module.ts
+++ src/modules/production/production.module.ts
@@ -1,7 +1,6 @@
 import { Module, Global } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ConsoleModule } from 'nestjs-console';
-import { ProductionTaskSagas } from './sagas/production-task.saga';
 import { ProduceProductCliController } from './commands/produce-product/produce-product.cli.controller';
 import { ProduceProductService } from './commands/produce-product/produce-product.service';
 import { UpdateSchedulerService } from '../monitor/commands/update-scheduler/update-scheduler.service';
@@ -11,20 +10,13 @@ const cliControllers = [ProduceProductCliController];
 
 const commandHandlers = [ProduceProductService, UpdateSchedulerService];
 
-const sagas = [ProductionTaskSagas];
-
 const customProviders = [productionServiceLoggerProvider];
 
 @Global()
 @Module({
   imports: [CqrsModule, ConsoleModule],
   controllers: [],
-  providers: [
-    ...cliControllers,
-    ...commandHandlers,
-    ...sagas,
-    ...customProviders,
-  ],
+  providers: [...cliControllers, ...commandHandlers, ...customProviders],
   exports: [...commandHandlers],
 })
 export class ProductionModule {}
