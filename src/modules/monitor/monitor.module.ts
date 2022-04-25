4c2e9564e99b43a2e3fb436c2e7f4514d0f05791
Ethan Zhang <ethan.zhang@ringcentral.com>
2022-04-25 17:32:47 +0800

    refactor(*) refactor

--- src/modules/monitor/monitor.module.ts
+++ src/modules/monitor/monitor.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { MonitorHttpController } from './queries/monitor.http.controller';
 import { MonitorQueryHandler } from './queries/monitor.query-handler';
-import { UpdatePipelineHttpController } from '@modules/monitor/commands/update-pipeline/update-pipeline-http.controller';
+import { UpdatePipelineHttpController } from '@modules/monitor/commands/update-pipeline/update-pipeline.http.controller';
 import { ProductionModule } from '@modules/production/production.module';
 
 const httpControllers = [MonitorHttpController, UpdatePipelineHttpController];
